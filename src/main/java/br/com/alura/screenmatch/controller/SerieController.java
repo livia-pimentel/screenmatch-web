c3a7395942f349970a42517642709bb8de079e1f
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-22 16:08:22 -0300

    Add anotação para não ficar repetindo o caminho /series

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,22 +4,24 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService servico;
 
-    @GetMapping("/series") // mapeia a rota
+    @GetMapping // mapeia a rota
     public List<SerieDTO> obterSeries() {
         return servico.obterTodasAsSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obterTop5Series() {
         return servico.obterTop5Series();
     }
