c0daeb417449c26e40ec04b0eac5ed24334dda06
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-22 09:46:48 -0300

    Add mapeamento de rota e testanto o /series com um texto

---
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+
+    @GetMapping("/series") // mapeia a rota
+    public String obterSeries() {
+        return "Aqui serão listas as séries";
+    }
+
+}
