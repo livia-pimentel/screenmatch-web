9add40471d806411f29c94542dd855e9ccb93f44
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-22 10:23:10 -0300

    Implementando o mapeamento de series, mas tem recursividade e o json aparece apenas no devtools

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,14 +1,23 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.alura.screenmatch.model.Serie;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series") // mapeia a rota
-    public String obterSeries() {
-        return "Aqui serão listas as séries";
+    public List<Serie> obterSeries() {
+        return repositorio.findAll();
     }
 
 }
