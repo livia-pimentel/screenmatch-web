f38866c69b200733ff57ba70b3514b890041ca18
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-24 10:51:27 -0300

    Add metodo para mostrar a serie por id

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,9 @@ public class SerieController {
         return servico.obterLancamento();
     }
 
-
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
+    }
 
 }
