c165b98480452f7182b0cc3806562a8f1fc53136
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-24 13:24:21 -0300

    Buscando todas as temporadas

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +38,8 @@ public class SerieController {
         return servico.obterPorId(id);
     }
 
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
+        return servico.obterTodasTemporadas(id);
+    }
 }
